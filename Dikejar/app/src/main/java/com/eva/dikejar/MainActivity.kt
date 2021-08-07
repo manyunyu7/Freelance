b772216bcf1e47b825d2486cfa1c90ffb6b5c7dc
henryaugusta <henryaugusta4@gmail.com>
2021-08-07 19:37:18 +0700

    Add Splash Screen

--- Dikejar/app/src/main/java/com/eva/dikejar/MainActivity.kt
+++ Dikejar/app/src/main/java/com/eva/dikejar/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.eva.dikejar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eva.dikejar.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        actionBar?.hide()
+        supportActionBar?.hide()
+
+        GlobalScope.launch {
+            delay(1500)
+            withContext(Dispatchers.IO) {
+                finish()
+                startActivity(Intent(this@MainActivity, NavigationControllerActivity::class.java))
+            }
+        }
     }
 }
